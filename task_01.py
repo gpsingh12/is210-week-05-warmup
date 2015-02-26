932004e5e2e496e7e2b98ffe509283f8b46d1870
gpsingh12 <gurpreet_1441@yahoo.com>
2015-02-26 23:48:08 +0000

    edits

--- task_01.py
+++ task_01.py
@@ -4,6 +4,17 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """Function takes two arguments
+
+       Args:
+           wink(str): Takes no default value.
+           numwink(int): Takes default value of 2.
+           winks(str): Arg is assigned vallues using mathematical computations.
+           restr(mixed): return the value corresponding to arg winks and nudges.
+       Return(str):
+               returns a string value assigned to retsr.
+
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
